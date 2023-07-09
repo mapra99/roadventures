8b7ac7a63c4d962064d35363d31429da48942f02
Miguel Prada <mapra99@gmail.com>
2023-07-09 08:42:56 -0500

    wip

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -2,26 +2,23 @@ import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, Text, View } from 'react-native'
 import * as SplashScreen from 'expo-splash-screen'
 import useCustomFonts from 'hooks/use-custom-fonts'
+import styled from 'styled-components/native'
 
 SplashScreen.preventAutoHideAsync()
+
+const StyledText = styled.Text`
+  font-family: 'Nunito Sans';
+  font-weight: 700;
+`
 
 export default function App() {
   const { fontsLoaded, hideSplash } = useCustomFonts()
   if (!fontsLoaded) return null
 
   return (
-    <View style={styles.container} onLayout={hideSplash}>
-      <Text style={{fontFamily: 'Nunito Sans'}} >Open up App.js to sasdastart working on youasdadr app!</Text>
+    <View onLayout={hideSplash}>
+      <StyledText>Open up App.js to sasdastart working on youasdadr app!</StyledText>
       <StatusBar style="auto" />
     </View>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-})
--- mobile/src/hooks/use-custom-fonts/index.ts
+++ mobile/src/hooks/use-custom-fonts/index.ts
@@ -4,7 +4,7 @@ import * as SplashScreen from 'expo-splash-screen'
 
 export default function useCustomFonts() {
   const [fontsLoaded] = useFonts({
-    'Nunito Sans': require('../../../assets/fonts/nunito-sans.ttf'),
+    'Nunito Sans': require('../../../assets/fonts/nunito-sans.ttf')
   })
 
   const hideSplash = useCallback(async () => {
