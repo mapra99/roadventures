de38dc8c46158f2cf82a77b0fae46265e5a40b06
Miguel Prada <mapra99@gmail.com>
2023-07-12 15:29:42 -0500

    finish auth service

--- mobile/src/services/auth.ts
+++ mobile/src/services/auth.ts
@@ -1,34 +1,43 @@
+import invariant from 'tiny-invariant'
 import { signUp, login, logout } from 'rest-client'
 import * as ss from './secure-store'
 
-import type { SignUpParams } from 'rest-client/auth/types'
+import type { SignUpErrors, SignUpParams } from 'rest-client/auth/types'
+import type { User } from 'rest-client/schemas'
 
 const ACCESS_TOKEN_KEY = 'accessToken'
 
-export async function signUpUser(params: SignUpParams) {
-  const [success, result] = await signUp(params)
-  if (!success) return [false, result]
+export async function signUpUser(params: SignUpParams): Promise<[true, User] | [false, SignUpErrors['errors']]> {
+  const [signUpSuccess, signUpResult] = await signUp(params)
+  if (!signUpSuccess) return [false, signUpResult]
 
-  return result
+  const [loginSuccess, loginResult] = await loginUser(params.email, params.password)
+  if (!loginSuccess) throw new Error(`User created but could not login: ${loginResult}`)
+
+  return [true, loginResult]
 }
 
-export async function loginUser(email: string, password: string) {
+export async function loginUser(email: string, password: string): Promise<[true, User] | [false, string]> {
   const [success, result] = await login({ email, password })
   if (!success) return [false, result]
 
-  const { accessToken } = result
+  const { accessToken, user } = result
   await ss.save(ACCESS_TOKEN_KEY, accessToken)
 
-  return [true, result]
+  return [true, user]
 }
 
 export async function logoutUser() {
   const accessToken = await loadAccessToken()
+  invariant(accessToken, 'No access token found')
+
   await ss.remove(ACCESS_TOKEN_KEY)
   await logout(accessToken)
 }
 
 export async function loadAccessToken() {
-  const token = await ss.load(ACCESS_TOKEN_KEY)
-  return token
+  const [success, token] = await ss.load(ACCESS_TOKEN_KEY)
+  if (success) return token
+
+  return undefined
 }
