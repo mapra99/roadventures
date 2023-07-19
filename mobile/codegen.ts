25e2ec73095f9e86011e9bde39a44a2b3892e77c
Miguel Prada <mapra99@gmail.com>
2023-07-18 22:52:45 -0500

    fix codegen

--- mobile/codegen.ts
+++ mobile/codegen.ts
@@ -1,13 +1,12 @@
-
 import type { CodegenConfig } from '@graphql-codegen/cli'
 
 const config: CodegenConfig = {
   overwrite: true,
   schema: 'http://localhost:3000/api/v1/graphql',
-  documents: 'src/**/*.ts',
+  documents: ['src/**/*.ts'],
   generates: {
     'src/gql-client/': {
-      plugins: []
+      preset: 'client'
     }
   }
 }
