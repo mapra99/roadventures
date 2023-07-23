046deea747e931eff3fba2c8dd11feeae73d08db
Miguel Prada <mapra99@gmail.com>
2023-07-22 19:28:48 -0500

    new trip button

--- mobile/src/screens/home/index.tsx
+++ mobile/src/screens/home/index.tsx
@@ -2,12 +2,18 @@ import Layout from 'components/layout'
 import ActiveTrips from 'components/active-trips'
 import PastTrips from 'components/past-trips'
 import { FullHeightScrollableBody, BodyWithPadding } from 'components/body'
+import { Button } from 'react-native'
+import COLORS from 'constants/colors'
+import { TopBarWrapper } from './styled'
 
 function Home() {
   return (
     <Layout>
       <FullHeightScrollableBody>
         <BodyWithPadding>
+          <TopBarWrapper>
+            <Button title="Start new trip" color={COLORS.PRIMARY} />
+          </TopBarWrapper>
           <ActiveTrips />
           <PastTrips />
         </BodyWithPadding>
---
+++ mobile/src/screens/home/styled.ts
@@ -0,0 +1,5 @@
+import styled from 'styled-components/native'
+
+export const TopBarWrapper = styled.View`
+  margin-bottom: 24px;
+`
