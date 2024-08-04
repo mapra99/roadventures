ed7df7e485bca6e8be8f6e1dae5cb019c665ea78
Miguel Prada <mapra99@gmail.com>
2024-08-04 18:19:10 -0500

    Mapbox PoC

--- mobile/src/screens/home/index.tsx
+++ mobile/src/screens/home/index.tsx
@@ -1,17 +1,38 @@
-import { useEffect } from 'react'
 import Layout from 'components/layout'
-import { BodyText } from 'components/text'
+import Mapbox, { Camera, LocationPuck, MapView, UserLocation } from '@rnmapbox/maps'
+import { StyleSheet, View } from 'react-native'
 
-import { removeAccessToken } from 'services/auth'
+Mapbox.setAccessToken('sk.eyJ1IjoibXByYWRhOTkiLCJhIjoiY2x6ZzF6cDVtMGowbTJyb2pnYXpnb2N6YiJ9.FZ0pjqDNTy8luWIpqsF5UA')
+
+const styles = StyleSheet.create({
+  page: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF'
+  },
+  container: {
+    height: '100%',
+    width: '100%',
+    backgroundColor: 'tomato'
+  },
+  map: {
+    flex: 1
+  }
+})
 
 function Home() {
-  useEffect(() => {
-    removeAccessToken() // TODO: this is just for testing purposes
-  }, [])
-
   return (
     <Layout>
-      <BodyText>HOLI USUARIO</BodyText>
+      <View style={styles.page}>
+        <View style={styles.container}>
+          <MapView style={styles.map} compassEnabled>
+            <Camera centerCoordinate={[-74.03312551130742, 4.732474949026318]} zoomLevel={15} />
+            <LocationPuck puckBearing="course" puckBearingEnabled />
+            <UserLocation />
+          </MapView>
+        </View>
+      </View>
     </Layout>
   )
 }
